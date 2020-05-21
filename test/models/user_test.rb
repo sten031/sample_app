370a5938a9d2c3b89c8fa4fcb26ba0dfc57d5843
shingo tanaka <stenstenktensten@gmail.com>
2020-05-21 08:06:05 +0000

    Add account activation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
   def setup
-    @user = User.new(name: "Example User",email:"user@example.com",
-              password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -71,6 +72,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end
